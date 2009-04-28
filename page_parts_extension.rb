59d8384c9c10e480eba8b2d93c6e62e2f0da816a
Josh French <josh@digitalpulp.com>
2009-04-28 16:47:46 -0400

    Content types additions

--- page_parts_extension.rb
+++ page_parts_extension.rb
@@ -11,6 +11,11 @@ class PagePartsExtension < Radiant::Extension
     Admin::PagesController.send(:include, PageParts::Admin::PagesControllerExtensions)
     Dir.glob(File.join(PagePartsExtension.root, %w(app models *.rb))).each { |f| require_dependency f }
     require 'page_parts/standard_tags'
+    if defined? ContentTypesExtension
+      admin.content_types.edit.add :parts_head, 'admin/content_types/parts_head_type', :after => 'name'
+      admin.content_types.edit.add :parts_body, 'admin/content_types/parts_body_type', :after => 'name'
+      admin.content_types.edit.add :popup, 'admin/content_types/type_select', :after => 'name_field' 
+    end
   end
   
   def deactivate
