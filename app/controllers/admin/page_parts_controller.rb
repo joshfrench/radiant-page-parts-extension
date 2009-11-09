5f4ac20a53c38914cdccae9dec618f977e923772
Alex Wallace <alex@digitalpulp.com>
2009-11-09 15:49:10 -0500

    cleaned up markup for saving

--- app/controllers/admin/page_parts_controller.rb
+++ app/controllers/admin/page_parts_controller.rb
@@ -1,8 +1,9 @@
 class Admin::PagePartsController < Admin::ResourceController
+  helper :page_parts
   # Need to use the PagePart initializer instead of delegating to the abstract model
   def create
     self.model = PagePart.new(params[model_symbol])
-    render :partial => model.partial_name, :object => model,
+    render :partial => "page_part", :object => model,
       :locals => {:page_part_counter => params[:index].to_i, :page_part => model}
   end
 end
