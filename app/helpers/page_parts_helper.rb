a7e14ba83795658863292a8a7d5eae1029e36764
Alex Wallace <alex@digitalpulp.com>
2009-11-09 13:18:25 -0500

    fixed markup to work correctly with tabcontrol

--- app/helpers/page_parts_helper.rb
+++ app/helpers/page_parts_helper.rb
@@ -1,6 +1,6 @@
 module PagePartsHelper
   def render_part(part,fields)
     name = content_tag(:div, content_tag(:label, part.name, :for => "part_#{part.name.to_slug}_content"), :class => 'page-part-name')
-    content_tag(:div, [name,fields].join, :class => 'page-part Field', :id => "page-#{part.name.to_slug}")
+    content_tag(:div, [name,fields].join, :class => 'page page-part Field', :id => "page-#{part.name.to_slug}", "data-caption" => part.name.to_slug)
   end
 end
