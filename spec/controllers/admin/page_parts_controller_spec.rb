0f385f787e35880f1e922b98c9ab869c28473b3a
Josh French <josh@digitalpulp.com>
2009-11-10 12:08:10 -0500

    Parts controller now renders a master partial

--- spec/controllers/admin/page_parts_controller_spec.rb
+++ spec/controllers/admin/page_parts_controller_spec.rb
@@ -7,11 +7,6 @@ describe Admin::PagePartsController do
     login_as :admin
   end
   
-  it "should render a partial for the subclass" do
-    xhr :post, :create, :page_part => { :name => "boolean", :page_part_type => "BooleanPagePart" }
-    response.should render_template('admin/page_parts/_boolean_page_part')
-  end
-  
   it "should assign a subclassed page part" do
     xhr :post, :create, :page_part => { :name => "boolean", :page_part_type => "BooleanPagePart" }
     attr = assigns(:page_part)
